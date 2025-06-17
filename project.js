6afaeb4663b0637a50497c002200a8eabe25c43e
Seleti Tuitupou <ss.tuitupou@gmail.com>
2025-06-16 18:31:04 -0700

    First commit

---
+++ project.js
@@ -0,0 +1,15 @@
+// 1. Deposit some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Giver user their winnings
+// 7. Play again
+
+
+
+const deposit = () => {
+
+}
+
+deposit()
