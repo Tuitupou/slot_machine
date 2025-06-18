39995b72d6ddd84f0936fc49f07bfff2c10faf4b
Seleti Tuitupou <ss.tuitupou@gmail.com>
2025-06-18 16:30:24 -0700

    Added functionality

--- project.js
+++ project.js
@@ -6,10 +6,47 @@
 // 6. Giver user their winnings
 // 7. Play again
 
-
+const prompt = require("prompt-sync")();
 
 const deposit = () => {
+    while (true) {
+        const depositAmount = prompt("Enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
 
-}
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log("Invalid deposit amount, try again.");
+        } else {
+            return numberDepositAmount;
+        }
+    }
+};
 
-deposit()
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberOfines = parseFloat(lines);
+
+        if (isNaN(numberOfines) || numberOfines <= 0 || numberOfines >= 3) {
+            console.log("Invalid number of lines, try again.");
+        } else {
+            return numberOfLines;
+        }
+    }
+};
+
+const getBet = (balance) => {
+     while (true) {
+        const bet = prompt("Enter the total bet: ");
+        const numberBet = parseFloat(bet);
+
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet >= balance) {
+            console.log("Invalid bet, try again.");
+        } else {
+            return numberBet;
+        }
+    }
+};
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+
